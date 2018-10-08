2bf6c18b70322e973eb24a060ed86b5b5b2da697
Mads Gadeberg <mads.gadeberg1995@gmail.com>
2018-10-08 20:41:02 +0200

    added two tests and added the implementation afterwards in BrandService

--- SpeakerShopApp.Core/ApplicationService/Impl/BrandService.cs
+++ SpeakerShopApp.Core/ApplicationService/Impl/BrandService.cs
@@ -1,14 +1,30 @@
 using System.Collections.Generic;
+using System.IO;
 using SpeakerShopApp.Core.ApplicationService.Service;
+using SpeakerShopApp.Core.DomainService;
 using SpeakerShopApp.Core.Entity;
 
 namespace SpeakerShopApp.Core.ApplicationService.Impl
 {
     public class BrandService : IBrandService
     {
+        private readonly IBrandRepository _brandRepository;
+        private readonly ISpeakerRepository _speakerRepository;
+        
+        public BrandService(IBrandRepository brandRepository, ISpeakerRepository speakerRepository)
+        {
+            _brandRepository = brandRepository;
+            _speakerRepository = speakerRepository;
+        }
+
         public Brand CreateBrand(Brand brand)
         {
-            throw new System.NotImplementedException();
+            if (string.IsNullOrEmpty(brand.SpeakerBrand))
+            {
+                throw new InvalidDataException("Can not create a brand without a name");
+            }
+            
+            return _brandRepository.CreateBrand(brand);
         }
 
         public Brand ReadBrandById(int id)
--- TestCore/ApplicationService/Service/BrandServiceTest.cs
+++ TestCore/ApplicationService/Service/BrandServiceTest.cs
@@ -1,14 +1,53 @@
 ﻿using System;
+using System.IO;
+using SpeakerShopApp.Core.DomainService;
 using Xunit;
+using Moq;
+using SpeakerShopApp.Core.ApplicationService.Impl;
+using SpeakerShopApp.Core.ApplicationService.Service;
+using SpeakerShopApp.Core.Entity;
 
 namespace TestCore.ApplicationService.Service
 {
     public class BrandServiceTest
     {
+        
         [Fact]
-        public void Test1()
+        public void CreateBrandWithMissingSpeakerBrandThrowsException()
         {
+            var brandRepo = new Mock<IBrandRepository>();
+            var speakerRepo = new  Mock<ISpeakerRepository>();
             
+            IBrandService brandService = new BrandService(brandRepo.Object, speakerRepo.Object);
+
+            var brand = new Brand();
+
+            Exception e = Assert.Throws<InvalidDataException>(() =>
+                brandService.CreateBrand(brand));
+            
+            Assert.Equal("Can not create a brand without a name", e.Message);
+        }
+
+        [Fact]
+        public void CreateBrandSecureRepositoryIsCalled()
+        {
+            var brandRepo = new Mock<IBrandRepository>();
+            var speakerRepo = new  Mock<ISpeakerRepository>();
+            
+            IBrandService brandService = new BrandService(brandRepo.Object, speakerRepo.Object);
+
+            var brand = new Brand()
+            {
+                SpeakerBrand = "Bose"
+            };
+            
+            var isCalled = false;
+            brandRepo.Setup(x => x.CreateBrand(brand)).Callback(() => isCalled = true);
+
+            brandService.CreateBrand(brand);
+            Assert.True(isCalled);
+
+
         }
     }
 }
