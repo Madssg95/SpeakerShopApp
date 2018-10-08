68ea9c7d754b69a000a445ed79c0791fa18bcb83
Morten Kristensen <morten@Mortens-MacBook-Pro.local>
2018-10-08 12:15:30 +0200

    ISpeakerRepository

--- SpeakerShopApp.Core/DomainService/ISpeakerRepository.cs
+++ SpeakerShopApp.Core/DomainService/ISpeakerRepository.cs
@@ -1,14 +1,22 @@
+using System.Collections.Generic;
+using SpeakerShopApp.Core.Entity;
+
 namespace SpeakerShopApp.Core.DomainService
 {
     public interface ISpeakerRepository
     {
-       //Create
-        
-        
+        //Create
+        Speaker CreateSpeaker(Speaker speaker);
+
         //Read
-        
+        IEnumerable<Speaker> ReadAllSpeakers();
+
+        Speaker ReadSpeakerById(int id);
+
         //Update
-        
+        Speaker UpdateSpeaker(Speaker speakerUpdate);
+
         //Delete
+        Speaker DeleteSpeaker(int id);
     }
 }
