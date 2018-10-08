19c749ed6dd535c975824d9cfc87522cebfc19cb
Morten Kristensen <morten@Mortens-MacBook-Pro.local>
2018-10-08 11:56:53 +0200

    Brand + Speaker entity

---
+++ SpeakerShopApp.Core.Entity/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeakerShopApp.Core.Entity
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+
+        public String SpeakerBrand { get; set; }
+
+        public List<Speaker> Speakers { get; set;}
+
+    }
+}
--- SpeakerShopApp.Core.Entity/Speaker.cs
+++ SpeakerShopApp.Core.Entity/Speaker.cs
@@ -3,8 +3,17 @@ namespace SpeakerShopApp.Core.Entity
 {
     public class Speaker
     {
-        public Speaker()
-        {
-        }
+        public int SpeakerId { get; set; }
+
+        public string SpeakerName { get; set; }
+
+        public string SpeakerDescription { get; set; }
+
+        public double Price { get; set; }
+
+        public string Color { get; set; }
+
+        public Brand SpeakerBrand { get; set; }
     }
+
 }
