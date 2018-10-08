94583aa8ecb9620a81da7555edd4473925aad297
Mads Gadeberg <mads.gadeberg1995@gmail.com>
2018-10-08 14:59:20 +0200

    implemented BrandService & SpeakerService

---
+++ SpeakerShopApp.Core/ApplicationService/Impl/BrandService.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using SpeakerShopApp.Core.ApplicationService.Service;
+using SpeakerShopApp.Core.Entity;
+
+namespace SpeakerShopApp.Core.ApplicationService.Impl
+{
+    public class BrandService : IBrandService
+    {
+        public Brand CreateBrand(Brand brand)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Brand ReadBrandById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Brand> ReadAllBrands()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Brand UpdateBrand(Brand brand)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Brand DeleteBrand(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SpeakerShopApp.Core/ApplicationService/Impl/SpeakerService.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using SpeakerShopApp.Core.ApplicationService.Service;
+using SpeakerShopApp.Core.Entity;
+
+namespace SpeakerShopApp.Core.ApplicationService.Impl
+{
+    public class SpeakerService : ISpeakerService
+    {
+        public Speaker CreateSpeaker(Speaker speaker)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Speaker ReadSpeakerById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Speaker> ReadAllSpeakers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Speaker UpdateSpeaker(Speaker speaker)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Speaker DeleteSpeaker(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
