5c83d2bd3b1f9def880ff3a3d12a567c9054c0f8
Mads Gadeberg <mads.gadeberg1995@gmail.com>
2018-10-09 14:24:11 +0200

    added the user class

---
+++ SpeakerShopApp.Core.Entity/User.cs
@@ -0,0 +1,11 @@
+namespace SpeakerShopApp.Core.Entity
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
